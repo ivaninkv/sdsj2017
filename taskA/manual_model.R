3b00e7b35e845c294c815994e20aea5938dca82f
Konstantin <ivanin.k@gmail.com>
2017-09-23 20:54:16 +0300

    lgb tuning

--- taskA/manual_model.R
+++ taskA/manual_model.R
@@ -3,8 +3,7 @@ gc()
 
 library(dstools)
 library(tidyverse)
-library(Matrix)
-library(mlr)
+library(lightgbm)
 
 train.data <- readr::read_rds('data/train.rds')
 test.data <- readr::read_rds('data/test.rds')
@@ -22,15 +21,6 @@ X <- as.data.frame(model.matrix(~. - 1, X))
 X$target <- as.factor(X$target)
 X_pred <- as.data.frame(model.matrix(~. - 1, X_pred))
 
-head(listLearners()[c('class', 'package')])
-
-task <- makeClassifTask(data = X, target = 'target')
-lrn <- makeLearner('classif.h2o.deeplearning', predict.type = 'prob')
-model <- train(lrn, task)
-readr::write_rds(model, 'model.h2o')
-res <- predict(model, newdata = X_pred)
-sample.submsission$prediction <- res[1]
-readr::write_csv(sample.submsission, 'data/h2o.csv')
 
 
 
